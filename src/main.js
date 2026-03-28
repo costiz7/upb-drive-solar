6df56a2f5e39439b638b3270fa838d33c62d5170
costiz7 <costelzanescu7@gmail.com>
2026-03-29 00:11:16 +0200

    Implemented NavBar

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './css/global.css';
+import { renderNavbar } from './components/NavBar.js';
+
+renderNavbar();
+
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  
+  if (navbar) {
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
+});
