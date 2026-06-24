c74c49ca3af0c3c3cc5fa1498df823bf22d81ef8
costiz7 <costelzanescu7@gmail.com>
2026-06-24 22:12:18 +0300

    no time for comment

--- src/Atoms/DepartmentSection/DepartmentSection.jsx
+++ src/Atoms/DepartmentSection/DepartmentSection.jsx
@@ -1,14 +1,14 @@
 import './DepartmentSection.css';
 
-const DepartmentSection = ({ className, imgPath, imgAlt, title, children }) => {
+const DepartmentSection = ({ className, imgPath, imgAlt, title, titleColor, isInverse, children }) => {
     return (
         <>
-        <div className={`department-wrapper ${className}`}>
+        <div className={`department-wrapper ${className} ${isInverse ? 'inverse' : ''}`}>
           <div className="department-image">
             <img src={imgPath} alt={imgAlt} />
           </div>
           <div className="department-content">
-            <h1>{title}</h1>
+            <h1 style={ { color: titleColor } }>{title}</h1>
             <p className="text-content">
               {children}
             </p>
--- src/MeetTheTeamPage/MeetTheTeamPage.jsx
+++ src/MeetTheTeamPage/MeetTheTeamPage.jsx
@@ -5,17 +5,14 @@ const MeetTheTeamPage = () => {
     return (
         <>
             <div className="mtt-wrapper">
-                <div className="upb-team-wrapper">
-                    <h1>The UPB DRIVE Solar Team</h1>
-                    <div className="img-wrapper">
-                        <img src="/images/IMG_16.JPG" alt="The UPb DRIVE Solar Team Photo" />
-                    </div>
-                    <div className="paragraphs">
-                        <p>Our team is composed of 38 students from various faculties and specializations, bringing together a diverse set of skills and perspectives. This multidisciplinary structure allows us to approach the project from both a strong technical and organizational standpoint.</p>
-                        <p>The team is divided into two main branches: <span>Technical</span> and <span>Operational</span>, each playing a critical role in the development and success of the project.</p>
-                    </div>
-                </div>
-                <DepartmentSection imgPath="/images/IMG_16.JPG" title="The UPB DRIVE Solar Team">Our team is composed of 38 students from various faculties and specializations, bringing together a diverse set of skills and perspectives. This multidisciplinary structure allows us to approach the project from both a strong technical and organizational standpoint.</DepartmentSection>
+                <DepartmentSection
+                    imgPath="/images/IMG_16.webp"
+                    title="The UPB DRIVE Solar Team"
+                    titleColor="var(--vibrant-rose)"
+                    isInverse={false}
+                >
+                    Our team is composed of 38 students from various faculties and specializations, bringing together a diverse set of skills and perspectives. This multidisciplinary structure allows us to approach the project from both a strong technical and organizational standpoint.<br /><br /> The team is divided into two main branches: Technical and Operational, each playing a critical role in the development and success of the project.
+                </DepartmentSection>
 
             </div>
         </>
