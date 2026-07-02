e4055c0919491b34233a7d09c9a535827c33e302
costiz7 <costelzanescu7@gmail.com>
2026-07-02 14:27:17 +0300

    Globalized fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
 import ScrollToTop from "./Atoms/ScrollToTop/ScrollToTop.jsx";
 import MainLayout from "./MainLayout/MainLayout.jsx";
 import HomePage from "./HomePage/HomePage.jsx";
